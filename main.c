08ceb66147074edd9f2943103bd8c6f2b4372de5
Cristian Ciobanu <cristian.ciobanu@amdaris.com>
2019-02-05 15:58:36 +0200

    To main add argumments read

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main()
+int main(int argc, char **argv)
 {
-	printf("Hello world\n");
+	if (argc != 1)
+		printf("Arguments\n");
+	else
+		printf("No Arguments\n");
 	return (0);
 }
